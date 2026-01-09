3f43bd809a23cf71ee8299c0cc76fa871c515534
lewykin1996 <lewykin1996@gmail.com>
2026-01-09 14:52:17 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	int a;
+	int sum = 0;
+
+	cout << "Введите целое число или число '0', чтобы закончить:" << endl;
+	cin >> a;
+
+	while (a != 0)
+	{
+		sum = sum + a;
+
+		cout << "Введите целое число или число '0', чтобы закончить:" << endl;
+		cin >> a;
+	}
+	cout << "Сумма:" << sum << endl;
+
+	return 0;
+}
+
+
